9e7c79a45a1fbb29828e42ec284a236458a48a70
Nicolas Leigh <linze201@gmail.com>
2025-01-05 19:22:30 +0800

    add database connection and storage layer; initialize PostgreSQL with Docker

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -7,14 +7,24 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/nicolasleigh/social/internal/store"
 )
 
 type application struct {
 	config config
+	store  store.Storage
 }
 
 type config struct {
 	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 func (app *application) mount() http.Handler {
@@ -22,14 +32,14 @@ func (app *application) mount() http.Handler {
 
 	// A good base middleware stack
 	r.Use(middleware.RequestID)
-  r.Use(middleware.RealIP)
-  r.Use(middleware.Logger)
-  r.Use(middleware.Recoverer)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	// Set a timeout value on the request context (ctx), that will signal
-  // through ctx.Done() that the request has timed out and further
-  // processing should be stopped.
-  r.Use(middleware.Timeout(60 * time.Second))
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"log"
 
+	"github.com/nicolasleigh/social/internal/db"
 	"github.com/nicolasleigh/social/internal/env"
+	"github.com/nicolasleigh/social/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool established!")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	log.Fatal(app.run(app.mount()))
