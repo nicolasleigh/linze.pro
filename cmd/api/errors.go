1c3653fefd9cb00ec2d90d79f666759c19b815d6
Nicolas Leigh <linze201@gmail.com>
2025-01-12 12:10:26 +0800

    implement basic authentication middleware and error handling

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -31,3 +31,16 @@ func (app *application) conflictError(w http.ResponseWriter, r *http.Request, er
 
 	writeJSONError(w, http.StatusConflict, err.Error())
 }
+
+func (app *application) unauthorizedError(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Warnw("unauthorized error", "method", r.Method, "path", r.URL.Path, "error", err)
+
+	writeJSONError(w, http.StatusUnauthorized, "unauthorized")
+}
+
+func (app *application) unauthorizedBasicError(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Warnw("unauthorized basic error", "method", r.Method, "path", r.URL.Path, "error", err)
+	w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+
+	writeJSONError(w, http.StatusUnauthorized, "unauthorized")
+}
