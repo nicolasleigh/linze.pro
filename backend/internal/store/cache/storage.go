2e7d222c6fef613ef2117039b4d71a969071106c
Nicolas Leigh <linze201@gmail.com>
2026-09-16 17:45:07 +0800

    feat(cache): implement view cache store and add mocks for testing

--- backend/internal/store/cache/storage.go
+++ backend/internal/store/cache/storage.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"time"
 
 	"github.com/nicolasleigh/social/internal/store"
 	"github.com/redis/go-redis/v9"
@@ -12,10 +13,19 @@ type Storage struct {
 		Get(context.Context, int64) (*store.User, error)
 		Set(context.Context, *store.User) error
 	}
+	Views interface {
+		Seen(context.Context, string, string, string) (bool, error)
+		MarkSeen(context.Context, string, string, string) error
+	}
+	RateLimits interface {
+		Allow(context.Context, string, int64, time.Duration) (bool, error)
+	}
 }
 
 func NewRedisStorage(rdb *redis.Client) Storage {
 	return Storage{
-		Users: &UserStore{rdb: rdb},
+		Users:      &UserStore{rdb: rdb},
+		Views:      &ViewStore{rdb: rdb},
+		RateLimits: &RateLimitStore{rdb: rdb},
 	}
 }
